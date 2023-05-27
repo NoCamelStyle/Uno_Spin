2da526112b1c36cb011da0de8bc1e0c13b0c1ca9
andreykormachenko <andreysxkormachenko@gmail.com>
2023-05-27 16:05:46 +0300

    design

--- app/src/main/java/nocamelstyle/uno/spin/Screen.kt
+++ app/src/main/java/nocamelstyle/uno/spin/Screen.kt
@@ -5,30 +5,62 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Screen(state: ScreenState, click: () -> Unit) {
     when (state) {
         ScreenState.Animation -> {
             Box(modifier = Modifier.fillMaxSize()) {
-                CircularProgressIndicator()
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
             }
         }
 
         is ScreenState.Card -> {
-            Row(Modifier.clickable { click() }) {
-                Column() {
-                    Text(text = state.item.shortTitle)
-                    Image(painter = painterResource(state.item.cover), contentDescription = null)
+            Row(Modifier.clickable { click() }, verticalAlignment = Alignment.CenterVertically) {
+                Spacer(modifier = Modifier.width(24.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = state.item.shortTitle,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Image(
+                        painter = painterResource(state.item.cover),
+                        contentDescription = null,
+                        modifier = Modifier.size(width = 200.dp, height = 200.dp)
+                    )
                 }
-                Text(text = state.item.longTitle)
+                Text(
+                    text = state.item.longTitle,
+                    Modifier.padding(24.dp).alpha(0.8f),
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp
+                )
             }
         }
     }
+}
+
+@Preview(showBackground = true, widthDp = 400, heightDp = 200)
+@Composable
+private fun ScreenPreview() {
+    Screen(ScreenState.Card(resultsVariant.first())) {}
 }
